9267821fa7429c62b49306b0015662c3ee6e9aaf
Hasan Masrur <mhbabu99@gmail.com>
2024-07-14 09:37:37 +0600

    otp send set validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateOtpDto } from './dto/otp.dto';
 import { Service } from 'src/common/service.common';
 import { Otp } from './schema/otp.schema';
@@ -9,28 +9,48 @@ import { OtpVerifyDto } from './dto/otpVerify.dto';
 @Injectable()
 export class AuthService extends Service<Otp> {
 
-    constructor (@InjectModel(Otp.name) OtpModel:Model<Otp>){
+    constructor(@InjectModel(Otp.name) OtpModel: Model<Otp>) {
         super(OtpModel);
     }
 
     async otpCreate(createOtpDto: CreateOtpDto) {
-
+        const user = await this.findAllByQuery({ phone_number: createOtpDto.phone_number, });
         const otp_code = Math.floor(100000 + Math.random() * 900000);
+        if (!user || !user.length) {
+
+        } else {
+            if (user[0].attempt_at <=10) {
+                const updateOtp = {
+                    otp_code: otp_code,
+                    time_limit: Date.now(),
+                    attempt_at: user[0].attempt_at + 1,
+                    fcm_token: createOtpDto.fcm_token,
+                    app_key: createOtpDto.app_key,
+                    device_id: createOtpDto.device_id,
+                    location: { coordinates: [createOtpDto.location.lng, createOtpDto.location.lat] }
+                }
+                return await this.updateById(user[0]._id, updateOtp);
+            } else {
+                throw new BadRequestException('User attemp limit already exist');
+            }
+
+        }
+
         const modifiedDto = {
             ...createOtpDto, otp_code: otp_code, time_limit: Date.now(), attempt_at: 1,
             location: { coordinates: [createOtpDto.location.lng, createOtpDto.location.lat] }
         }
         return await this.createOne(modifiedDto);
     }
-    async otpVerify(otpVerifyDto:OtpVerifyDto) {
-        const otp = await this.findAllByQuery({phone_number:otpVerifyDto.phone_number,otp_code:otpVerifyDto.otp_code});
-       if(otp.length>0){
-        return otp[0] ;
-       }else {
-        return {
-            "message": "The OTP you entered is incorrect. Please try again"
-        };
-       }
+    async otpVerify(otpVerifyDto: OtpVerifyDto) {
+        const otp = await this.findAllByQuery({ phone_number: otpVerifyDto.phone_number, otp_code: otpVerifyDto.otp_code });
+        if (otp.length > 0) {
+            return otp[0];
+        } else {
+            return {
+                "message": "The OTP you entered is incorrect. Please try again"
+            };
+        }
         return otp;
     }
 }
